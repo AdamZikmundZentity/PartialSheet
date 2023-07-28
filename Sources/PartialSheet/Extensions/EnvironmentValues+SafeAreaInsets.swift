7b327bce38c58693232f050cf12b976f6889712b
Mike Ciesielka <michaelciesielka@producthunt.com>
2023-07-28 10:49:31 -0400

    Use different top inset for partial sheets presented on sheet presentations

--- Sources/PartialSheet/Extensions/EnvironmentValues+SafeAreaInsets.swift
+++ Sources/PartialSheet/Extensions/EnvironmentValues+SafeAreaInsets.swift
@@ -15,7 +15,11 @@ extension EnvironmentValues {
 
 private struct SafeAreaInsetsKey: EnvironmentKey {
     static var defaultValue: EdgeInsets {
-        UIApplication.shared.keyWindow?.safeAreaInsets.swiftUiInsets ?? EdgeInsets()
+        var insets = UIApplication.shared.keyWindow?.safeAreaInsets.swiftUiInsets ?? EdgeInsets()
+        if UIApplication.shared.statusBarHeightOverlap == 0 {
+            insets.top = 0
+        }
+        return insets
     }
 }
 
@@ -25,6 +29,40 @@ private extension UIApplication {
             .compactMap {  $0 as? UIWindowScene }
             .flatMap { $0.windows }
             .first { $0.isKeyWindow }
+    }
+
+    var statusBarHeightOverlap: CGFloat {
+        guard let topViewController = topViewController,
+              let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let window = windowScene.windows.first(where: \.isKeyWindow),
+              let statusBarManager = windowScene.statusBarManager else {
+            return 0
+        }
+
+        let statusBarFrame = statusBarManager.statusBarFrame
+        let convertedViewFrame = topViewController.view.convert(topViewController.view.bounds, to: window)
+        return statusBarFrame.intersection(convertedViewFrame).height
+    }
+
+    var topViewController: UIViewController? {
+        guard let window = topWindow else {
+            return nil
+        }
+
+        var top = window.rootViewController
+        while top?.presentedViewController != nil {
+            top = top?.presentedViewController
+        }
+        return top
+    }
+
+    var topWindow: UIWindow? {
+        guard let scene = connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene,
+              let window = scene.windows.first(where: \.isKeyWindow) else {
+            return nil
+        }
+
+        return window
     }
 }
 
